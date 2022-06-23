eaccd3378dae1d8a1bd483d79f2b7b5d11cb1b78
주이식 <helios2sic@gmail.com>
2022-06-23 22:05:45 +0900

    Feature: Error 페이지 기능

--- app/src/main/java/preview/android/PreviewApplication.kt
+++ app/src/main/java/preview/android/PreviewApplication.kt
@@ -1,9 +1,24 @@
 package preview.android
 
 import android.app.Application
+import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
+import preview.android.exception.PreviewExceptionHandler
+
 
 @HiltAndroidApp
-class PreviewApplication : Application(){
+class PreviewApplication : MultiDexApplication(){
 
+    override fun onCreate() {
+        super.onCreate()
+        setCrashHandler()
+    }
+
+    private fun setCrashHandler(){
+
+        val crashlyticsExceptionHandler = Thread.getDefaultUncaughtExceptionHandler() ?: return
+        Thread.setDefaultUncaughtExceptionHandler(
+            PreviewExceptionHandler(this)
+        )
+    }
 }
--- app/src/main/java/preview/android/activity/main/MainActivity.kt
+++ app/src/main/java/preview/android/activity/main/MainActivity.kt
@@ -1,24 +1,30 @@
 package preview.android.activity.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import dagger.hilt.android.AndroidEntryPoint
 import jkey20.errs.base.BaseActivity
 import preview.android.R
 import preview.android.databinding.ActivityMainBinding
+import java.lang.RuntimeException
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.activity_main) {
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(
+    R.layout.activity_main
+) {
 
     override val vm: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding.errorBtn.setOnClickListener{
-            throw Exception("Error Test")
+        binding.errorBtn.setOnClickListener {
+            throw RuntimeException("Error test")
         }
     }
+
 
 }
