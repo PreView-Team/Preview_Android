aea14b4517676c5e554e6ed514729f414722e053
주이식 <helios2sic@gmail.com>
2022-07-08 21:40:24 +0900

    Feature: FCM 수신 구현

--- app/src/main/java/preview/android/activity/util/Util.kt
+++ app/src/main/java/preview/android/activity/util/Util.kt
@@ -2,8 +2,14 @@ package preview.android.activity.util
 
 import android.content.Context
 import android.provider.Settings
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import preview.android.model.Mentor
 import preview.android.model.Post
+
+
+var deviceToken = ""
 
 fun createBestPostList(): List<Post> {
     val list = arrayListOf<Post>()
@@ -91,4 +97,24 @@ fun createMentorList(): List<Mentor> {
     )
 
     return list
+}
+
+fun getToken() : String{
+    if(deviceToken == "") {
+        var task =
+            FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.e("token ", "not successful")
+                    return@OnCompleteListener
+                }
+
+                // Get new FCM registration token
+                deviceToken = task.result
+            })
+        if (task.isSuccessful) {
+            deviceToken = task.result
+        }
+        Log.e("token: ", deviceToken.toString())
+    }
+    return deviceToken
 }
