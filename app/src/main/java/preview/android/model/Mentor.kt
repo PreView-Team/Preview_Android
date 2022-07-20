277e463d29ec2d61f6e1c6866684f603235dfb0c
주이식 <helios2sic@gmail.com>
2022-07-20 18:48:19 +0900

    refactor: 멘토 데이터 수정 및 바인딩처리

--- app/src/main/java/preview/android/model/Mentor.kt
+++ app/src/main/java/preview/android/model/Mentor.kt
@@ -3,10 +3,9 @@ package preview.android.model
 import java.io.Serializable
 
 data class Mentor(
-    val nickname : String = "",
-    val tag : String = "",
-    val title : String = "",
-    val content : String = "",
-    val likes : Int = 0,
-    val comments : Int = 0
+    val title : String ="",
+    val subTitle : String = "",
+    val contents : String = "",
+    val categortId : Int = 1,
+    val kakaoId : Int = 0,
 ) : Serializable
