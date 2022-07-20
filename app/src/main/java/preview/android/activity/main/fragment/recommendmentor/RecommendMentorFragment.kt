fe3a01b2698dd5eed0f6912ffb972b6bfed38584
주이식 <helios2sic@gmail.com>
2022-07-20 12:44:03 +0900

    Design: 추천멘토 페이지 디자인

---
+++ app/src/main/java/preview/android/activity/main/fragment/recommendmentor/RecommendMentorFragment.kt
@@ -0,0 +1,67 @@
+package preview.android.activity.main.fragment.recommendmentor
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.google.android.material.tabs.TabLayout
+import preview.android.BaseFragment
+import preview.android.R
+import preview.android.activity.main.MainViewModel
+import preview.android.activity.main.fragment.home.HomeViewModel
+import preview.android.activity.main.fragment.newmentor.NewMentorAdapter
+import preview.android.activity.mentorinfo.MentorInfoActivity
+import preview.android.data.MentorStore
+import preview.android.databinding.FragmentRecommendMentorBinding
+
+class RecommendMentorFragment : BaseFragment<FragmentRecommendMentorBinding, HomeViewModel>(
+    R.layout.fragment_recommend_mentor
+) {
+    override val vm: HomeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> {
+                    }
+
+                    1 -> {
+                    }
+
+                    2 -> {
+                    }
+
+                    3 -> {
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
+
+        binding.rvRecommendMentor.run {
+            setHasFixedSize(true)
+            setItemViewCacheSize(10)
+            adapter = RecommendMentorAdapter(
+                onApplyButtonClicked = { mentor ->
+                    val intent = Intent(context, MentorInfoActivity::class.java)
+                    intent.putExtra("mentorInfo", mentor)
+                    startActivity(intent)
+                }
+            ).apply {
+                submitList(MentorStore.recommendMentorList.value)
+            }
+        }
+    }
+}
