80a7ff7f502e46234e3d8512021273e1263eb9c3
cyjadela <adelacho@naver.com>
2022-07-27 11:18:23 +0900

    feature: 로그인 유지 구현

--- app/src/main/java/preview/android/activity/login/LoginActivity.kt
+++ app/src/main/java/preview/android/activity/login/LoginActivity.kt
@@ -1,18 +1,22 @@
 package preview.android.activity.login
 
 import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
 import com.kakao.sdk.common.util.Utility
 import dagger.hilt.android.AndroidEntryPoint
+import org.json.JSONArray
 import preview.android.BaseActivity
 import preview.android.R
 import preview.android.activity.main.MainActivity
 import preview.android.activity.util.ERROR_CODE_400
 import preview.android.data.AccountStore
 import preview.android.databinding.ActivityLoginBinding
+import preview.android.model.Account
 
 @AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(
@@ -27,19 +31,58 @@ class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(
         var keyHash = Utility.getKeyHash(this)
         Log.e(TAG, "해시 키 값 : ${keyHash}")
 
+        val pref = getSharedPreferences("loginAccount", MODE_PRIVATE)
+
+        val savedKakaoAccessToken = pref.getString("kakaoAccessToken", "").toString()
+        val savedNickname = pref.getString("nickname", "").toString()
+        val jobs = pref.getStringSet("job", null)
+        val savedJobs = ArrayList<String>()
+
+        if (jobs != null) {
+            for (i in jobs) {
+                savedJobs.add(i)
+            }
+        }
+
+        if (savedKakaoAccessToken.equals("")) {
+            Log.e("SharedPreferences", "저장 안됨")
+        } else {
+            Log.e("SPF", "저장 되어 있음 $savedKakaoAccessToken $savedNickname $savedJobs ")
+
+            AccountStore.updateNickname(savedNickname)
+
+            vm.setAccount(Account(savedKakaoAccessToken, savedNickname, savedJobs))
+            vm.loginToServer(vm.loadAccount())
+
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
         binding.btnKakao.setOnClickListener {
             vm.loginKaKao(this)
         }
 
         vm.account.observe(this) { account ->
             Log.e("ACCOUNT", account.toString())
+
+            val edit = pref.edit()
+            val set: Set<String> = account.jobNames.toSet()
+
+            edit.putString("kakaoAccessToken", account.kakaoAccessToken)
+            edit.putString("nickname", account.nickname)
+            edit.putStringSet("job", set)
+            edit.commit()
+
+            AccountStore.updateNickname(account.nickname)
+
             vm.loginToServer(account)
         }
 
         vm.responseResult.observe(this) { responseResult ->
 
             if (responseResult == ERROR_CODE_400) {
-                supportFragmentManager.beginTransaction().replace(R.id.layout_login, InfoInputFragment()).commit()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.layout_login, InfoInputFragment()).commit()
             } else {
                 AccountStore.updateToken(responseResult)
                 startActivity(Intent(this, MainActivity::class.java))
