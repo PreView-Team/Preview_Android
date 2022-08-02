51620c6a6a3a120363fe935e3cb16effb6b40a52
주이식 <helios2sic@gmail.com>
2022-08-02 11:25:26 +0900

    refactor: 채팅 관련 코드 리팩토링

--- app/src/main/java/preview/android/activity/management/chat/ChatViewModel.kt
+++ app/src/main/java/preview/android/activity/management/chat/ChatViewModel.kt
@@ -1,18 +1,22 @@
 package preview.android.activity.management.chat
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import preview.android.BaseViewModel
 import preview.android.activity.util.MutableListLiveData
 import preview.android.model.Message
 import preview.android.model.ReceiveFormThumbnail
+import preview.android.repository.ChatRepository
+import javax.inject.Inject
 
-class ChatViewModel : BaseViewModel() {
+@HiltViewModel
+class ChatViewModel @Inject constructor(
+    private val chatRepository: ChatRepository
+) : BaseViewModel() {
 
     private val _messageList = MutableStateFlow<List<Message>>(listOf())
     val messageList = _messageList.asStateFlow()
@@ -27,5 +31,25 @@ class ChatViewModel : BaseViewModel() {
     fun updateChatRoomList(list: List<String>) {
         _chatRoomList.clear()
         _chatRoomList.addAll(list)
+    }
+
+    fun readChatRoomList() = viewModelScope.launch {
+        chatRepository.readChatRoom().collect { list ->
+            Log.e("readChatRoomList", list.toString())
+            updateChatRoomList(list)
+        }
+    }
+
+    fun readChatList(nickname: String) = viewModelScope.launch {
+        chatRepository.readChat(nickname).collect { list ->
+            Log.e("readChatList", list.toString())
+            updateMessageList(list)
+        }
+    }
+
+    fun sendChat(nickname: String, message: Message, count: Int) = viewModelScope.launch {
+        chatRepository.sendChat(nickname, message, count).collect {
+            Log.e("response", it)
+        }
     }
 }
